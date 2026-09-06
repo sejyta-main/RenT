2a1f3fb361e41914f98028bfb413e8229fcd5fab
sejyta-main <sejyta@gmail.com>
2026-09-06 10:42:12 +0200

    Add offline service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='rent-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./assets/icon.svg','./assets/logo.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match('./index.html'))));});
